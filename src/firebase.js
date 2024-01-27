0a5f9799b3fc026427b1e60c792c58748e063f17
Syed Abdul Rehman <syedabdulrehman05@gmail.com>
2024-01-27 12:48:08 +0500

    update

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore/lite";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDY_dATQ02uPAgAYKvbOLsF_rDqGGjPV_o",
-  authDomain: "books-management-275ca.firebaseapp.com",
-  projectId: "books-management-275ca",
-  storageBucket: "books-management-275ca.appspot.com",
-  messagingSenderId: "220588650165",
-  appId: "1:220588650165:web:6c4214668b02f3b892d8fc",
-  measurementId: "G-ZPSKWQLG22",
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.Auth_Domain,
+  projectId: process.env.Project_Id,
+  storageBucket: process.env.Storage_Bucket,
+  messagingSenderId: process.env.Messaging_SenderId,
+  appId: process.env.App_Id,
+  measurementId: process.env.Mesurement_Id,
 };
 
 initializeApp(firebaseConfig);
